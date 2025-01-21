039ab7c58376e05202efc19bd05807ed7106461c
tanveer941 <tanveer941@gmail.com>
2025-01-21 15:07:09 +0530

    implementation for schema and target table/view name

--- examiner/macros/generate_schema_name.sql
+++ examiner/macros/generate_schema_name.sql
@@ -1,5 +1,17 @@
 {%- macro generate_schema_name(custom_schema_name, node) -%}
 
-    {{  adp_utils.generate_schema_name(custom_schema_name, node) }}
+    {%- set dev_targets = ('dev') -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
 
+        {{ default_schema | trim }}
+
+    {%- elif target.name in dev_targets -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
 {%- endmacro -%}
--- examiner/macros/get_custom_alias.sql
+++ examiner/macros/get_custom_alias.sql
@@ -1,5 +1,23 @@
 {%- macro generate_alias_name(custom_alias_name=none, node=none) -%}
 
-    {{ adp_utils.generate_alias_name(custom_alias_name, node) }}
+    {%- set skip_targets = ('test') -%}
+
+    {%- if custom_alias_name is none or target.name in skip_targets -%}
+
+        {{ node.name }}
+
+    {%- elif node.package_name == 'elementary' -%}
+
+        {{ return(custom_alias_name ~ "_" ~ node.name) }}
+
+    {%- elif node.version -%}
+
+        {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}
+
+    {%- else -%}
+
+        {{ custom_alias_name | trim }}
+
+    {%- endif -%}
 
 {%- endmacro -%}
