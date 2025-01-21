8c2f13f1fedd35bea25f5b65f8f715e462a2820f
tanveer941 <tanveer941@gmail.com>
2025-01-21 15:02:57 +0530

    override the schema and alias name

---
+++ examiner/macros/generate_schema_name.sql
@@ -0,0 +1,5 @@
+{%- macro generate_schema_name(custom_schema_name, node) -%}
+
+    {{  adp_utils.generate_schema_name(custom_schema_name, node) }}
+
+{%- endmacro -%}
---
+++ examiner/macros/get_custom_alias.sql
@@ -0,0 +1,5 @@
+{%- macro generate_alias_name(custom_alias_name=none, node=none) -%}
+
+    {{ adp_utils.generate_alias_name(custom_alias_name, node) }}
+
+{%- endmacro -%}
